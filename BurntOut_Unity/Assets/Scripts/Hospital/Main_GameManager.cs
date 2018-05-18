c892fb44606abd78ecd35cc63d9cbd4fd517d8ba
voltairemedina <medina@clinicaltools.com>
2018-05-18 15:34:36 -0400

    UI & Main Menu work
    + initial Main Menu
    + start button functionality
    + pause screen

--- BurntOut_Unity/Assets/Scripts/Hospital/Main_GameManager.cs
+++ BurntOut_Unity/Assets/Scripts/Hospital/Main_GameManager.cs
@@ -7,16 +7,36 @@ public class Main_GameManager : MonoBehaviour {
     public InteractableDoor room1;
     public InteractableDoor room2;
 
+    public GameObject Canvas_Paused;
+
+    public bool gamePaused;
+
     void Start () {
 
  
 
     }
 
-    void Update () {
+    void Update() {
 
         if (room1.completed == true && room2.completed == true) {
             Debug.Log("Game Completed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+
+            if (gamePaused == true) {
+                Debug.Log("Resume Game");
+                Time.timeScale = 1;
+                gamePaused = false;
+                Canvas_Paused.SetActive(false);
+            } else {
+                Debug.Log("Game Pause");
+                Time.timeScale = 0;
+                gamePaused = true;
+                Canvas_Paused.SetActive(true);
+            }
+
         }
 
 	}
---
+++ BurntOut_Unity/Assets/Scripts/MainMenu_GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void StartGame() {
+
+        Application.LoadLevel("Hospital");
+
+    }
+}
