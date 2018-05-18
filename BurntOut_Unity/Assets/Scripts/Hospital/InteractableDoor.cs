38f1434b427095b04ca070bf5b3911c86ea6d135
voltairemedina <medina@clinicaltools.com>
2018-05-18 14:56:15 -0400

    Hospital scene work with interactable objects, win condition & 1st person mechanics
    + basic 1st person mechanics
    + basic environment
    + interactable doors
    + game manager & win condition

---
+++ BurntOut_Unity/Assets/Scripts/Hospital/InteractableDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableDoor : MonoBehaviour {
+
+    
+    public int doorNumber;
+    public bool completed;
+    public bool isAroundDoor;
+
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+
+            if (isAroundDoor == true) {
+                Debug.Log("Door open");
+                completed = true;
+
+            }
+
+        }
+
+    }
+
+    void OnTriggerEnter (Collider col) {
+
+        if (col.gameObject.name == "Player") {
+
+            Debug.Log("Player Detected");
+            isAroundDoor = true;
+            //completed = true;
+
+        } else {
+
+            Debug.Log("Other obj detected");
+            isAroundDoor = false;
+
+        }
+
+    } 
+
+    void OnTriggerExit (Collider col) {
+
+        Debug.Log("Player away from door");
+        isAroundDoor = false;
+
+    }
+}
---
+++ BurntOut_Unity/Assets/Scripts/Hospital/Main_GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_GameManager : MonoBehaviour {
+
+    public InteractableDoor room1;
+    public InteractableDoor room2;
+
+    void Start () {
+
+ 
+
+    }
+
+    void Update () {
+
+        if (room1.completed == true && room2.completed == true) {
+            Debug.Log("Game Completed");
+        }
+
+	}
+
+
+}
