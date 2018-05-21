a18fc0864e7b56e099f0526c08388b593a41d8c0
voltairemedina <medina@clinicaltools.com>
2018-05-21 16:54:11 -0400

    Hospital/Gameplay - Added choice data on buttons for later functionality

---
+++ BurntOut_Unity/Assets/Scripts/UI/Choice/ChoiceData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceData : MonoBehaviour {
+
+    public string text;
+    public float result;
+
+    public PlayerStats stats;
+    public NarrativeManager narrativeManager;
+
+    private Text textComponent;
+
+    public void Start() {
+
+        textComponent = GetComponentInChildren<Text>();
+        UpdateText();
+
+    }
+
+    public void ApplyResult() {
+
+        stats.currentHealth += result;
+
+    }
+
+    public void UpdateText() {
+
+        textComponent.text = text;
+        
+    }
+}
