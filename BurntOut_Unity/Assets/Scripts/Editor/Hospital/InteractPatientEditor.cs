0b5cbe45cd1bbcfac2f84f4a35bb8db0bced89c6
Chris Davis <davis@clincaltools.com>
2018-05-23 15:55:47 -0400

    Add actor dropdown in the patient's inspector

---
+++ BurntOut_Unity/Assets/Scripts/Editor/Hospital/InteractPatientEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(Interact_Patient))]
+[CanEditMultipleObjects]
+public class InteractPatientEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var patient = (Interact_Patient)target;
+
+        var sceneNarrative = GameObject.Find("NarrativeManager").GetComponent<NarrativeManager>().sceneNarrative;
+        var scenario = sceneNarrative.GetScenario(patient.GetComponentInParent<Room>().scenarioId);
+
+        // Allows the scene to save changes and 'undo' to be possible
+        Undo.RecordObject(target, "Patient change");
+
+        var actorIndex = EditorGUILayout.Popup("Patient", scenario.ActorIndex(patient.patientId), scenario.ActorNames());
+        if (actorIndex >= 0)
+            patient.patientId = scenario.Actors[actorIndex].id;
+
+        patient.pressButton = (Text)EditorGUILayout.ObjectField("Press Button", patient.pressButton, typeof(Text), true);
+        patient.player = (GameObject)EditorGUILayout.ObjectField("Player", patient.player, typeof(GameObject), true);
+        patient.gameManager = (Main_GameManager)EditorGUILayout.ObjectField("Game Manager", patient.gameManager, typeof(Main_GameManager), true);
+        patient.dialogueManager = (DialogueManager)EditorGUILayout.ObjectField("Dialogue Manage:", patient.dialogueManager, typeof(DialogueManager), true);
+
+        patient.maxAngle = EditorGUILayout.FloatField("Max Angle", patient.maxAngle);
+    }
+}
--- BurntOut_Unity/Assets/Scripts/Editor/Hospital/RoomEditor.cs
+++ BurntOut_Unity/Assets/Scripts/Editor/Hospital/RoomEditor.cs
@@ -13,11 +13,11 @@ public class RoomEditor : Editor
         // Allows the scene to save changes and 'undo' to be possible
         Undo.RecordObject(target, "Room change");
 
-        room.dialogueManager = (DialogueManager) EditorGUILayout.ObjectField(room.dialogueManager, typeof(DialogueManager), true);
         var sceneNarrative = GameObject.Find("NarrativeManager").GetComponent<NarrativeManager>().sceneNarrative;
-
-        var scenarioIndex = EditorGUILayout.Popup(sceneNarrative.ScenarioIndex(room.scenarioId), sceneNarrative.ScenarioNames());
+        var scenarioIndex = EditorGUILayout.Popup("Scenario", sceneNarrative.ScenarioIndex(room.scenarioId), sceneNarrative.ScenarioNames());
         if (scenarioIndex >= 0)
             room.scenarioId = sceneNarrative.scenarios[scenarioIndex].id;
+
+        room.dialogueManager = (DialogueManager) EditorGUILayout.ObjectField("Dialogue Manager", room.dialogueManager, typeof(DialogueManager), true);
     }
 }
