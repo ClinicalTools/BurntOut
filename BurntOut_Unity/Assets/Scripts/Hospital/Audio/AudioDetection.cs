e5e5a2616900753d01366a53bb6bf0547510a148
voltairemedina <medina@clinicaltools.com>
2018-05-25 11:11:32 -0400

    Hallway now has audio, will lerp volume in and out of zone
    - controlled through AudioDetection.cs (Collider detection) and Animations lerping the volume

---
+++ BurntOut_Unity/Assets/Scripts/Hospital/Audio/AudioDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDetection : MonoBehaviour {
+
+    public Animator myAnimator;
+
+    // detect if player is within interact range
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.name == "Player") {
+            myAnimator.SetBool("PlayerInZone", true);
+        }
+
+
+    }
+
+    // return to default state when out of range
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.name == "Player") {
+            myAnimator.SetBool("PlayerInZone", false);
+        }
+        
+    }
+}
