9dc42a4484e0a2395ce030a1e75bf49c1b220bec
Chris Davis <davis@clincaltools.com>
2018-05-30 17:16:58 -0400

    Player Y cannot move

---
+++ BurntOut_Unity/Assets/Scripts/Player/SetY.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetY : MonoBehaviour {
+    public float y;
+	// Update is called once per frame
+	void Update () {
+        var pos = transform.position;
+        pos.y = y;
+        transform.position = pos;
+	}
+}
