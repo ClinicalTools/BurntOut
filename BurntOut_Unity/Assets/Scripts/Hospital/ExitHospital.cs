44d27e2ccb18391c03b355293b3afa7f7586541a
voltairemedina <medina@clinicaltools.com>
2018-05-30 09:27:47 -0400

    win changed, now an intractable door that checks each condition
    - if false, displays "you cant leave yet"

--- BurntOut_Unity/Assets/Scripts/Hospital/ExitHospital.cs
+++ BurntOut_Unity/Assets/Scripts/Hospital/ExitHospital.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitHospital : MonoBehaviour {
 
@@ -9,25 +10,74 @@ public class ExitHospital : MonoBehaviour {
     public GameObject Canvas_Win;
     public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public bool isAroundStation;
+    public bool playerFacing;
 
-        
-	}
+    public Text interactPrompt;
 
-    private void OnTriggerEnter(Collider other) {
-        
-        if (other.tag == "Player" && gm.hospitalwin == true) {
+    public float maxAngle = 35;
+
+    void Update() {
+
+        if (isAroundStation) {
+            Vector3 vec = transform.position - player.transform.position;
+            vec.y = 0;
+
+            // Measure from the direction opposite the player
+            Vector3 playerDir = player.transform.TransformDirection(Vector3.forward);
+            playerDir.y = 0;
+
+            bool wasFacing = playerFacing;
+            playerFacing = Vector3.Angle(playerDir, vec) < maxAngle;
+
+            if (wasFacing && !playerFacing)
+                LookAway();
+            else if (!wasFacing && playerFacing)
+                Look();
+        }
+
+        // if player is around patient, allow player to interact with it
+        if (Input.GetKeyDown(KeyCode.E) && isAroundStation && playerFacing && gm.hospitalwin) {
+            interactPrompt.transform.parent.gameObject.SetActive(false);
+
+            // INTERACTION HERE
             Debug.Log("Game Completed");
             //Time.timeScale = 0;
             Canvas_Win.SetActive(true);
             player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+
+            player.GetComponentInChildren<PlayerRotateToTarget>().target = gameObject;
+            player.GetComponentInChildren<PlayerRotateToTarget>().enabled = true;
+
+        }
+    }
+
+    private void Look() {
+        interactPrompt.transform.parent.gameObject.SetActive(true);
+        interactPrompt.text = "Press 'e' to leave hospital";
+
+        if (!gm.hospitalwin)
+            interactPrompt.text = "You cannot leave yet!";
+    }
+
+    private void LookAway() {
+        interactPrompt.transform.parent.gameObject.SetActive(false);
+    }
+
+    // detect if player is within interact range
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.name == "Player")
+            isAroundStation = true;
+    }
+
+    // return to default state when out of range
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.name == "Player") {
+            isAroundStation = false;
+            playerFacing = false;
         }
 
+        LookAway();
     }
+
 }
