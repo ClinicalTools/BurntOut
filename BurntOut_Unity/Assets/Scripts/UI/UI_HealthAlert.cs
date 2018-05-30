49d0d5b9f00cc6869cb14ea864db9e7e298d37ee
voltairemedina <medina@clinicaltools.com>
2018-05-30 09:55:34 -0400

    Added visual for low health
    - currently <= 40 health
    - pulsating alpha using animation

---
+++ BurntOut_Unity/Assets/Scripts/UI/UI_HealthAlert.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HealthAlert : MonoBehaviour {
+
+    public PlayerStats playerstats;
+    public GameObject mytext;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (playerstats.CurrentHealth < 40) {
+            mytext.SetActive(true);
+        } else
+            mytext.SetActive(false);
+
+
+    }
+}
