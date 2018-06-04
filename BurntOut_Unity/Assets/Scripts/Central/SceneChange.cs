a6a1c68931abf9822b55ce22a431460c8397a55d
voltairemedina <medina@clinicaltools.com>
2018-06-04 13:10:34 -0400

    New scene "Central" as Central area for level selection

---
+++ BurntOut_Unity/Assets/Scripts/Central/SceneChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class SceneChange : MonoBehaviour {
+
+
+    public string level;
+    public Animator screenfade;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other) {
+
+
+        if (other.name == "Player") {
+            StartCoroutine(Transition());
+        }
+
+    }
+
+    public IEnumerator Transition() {
+
+        player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+        screenfade.SetBool("fade", true);
+        yield return new WaitForSeconds(0.5f);
+        Application.LoadLevel(level);
+    }
+
+}
