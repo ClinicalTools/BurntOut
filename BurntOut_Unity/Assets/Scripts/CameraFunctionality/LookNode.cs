6ab243b76d27d59be628006d1e738f1a9bc3a90e
voltairemedina <medina@clinicaltools.com>
2018-06-06 10:42:33 -0400

    Began incorporating "point & click" movement
    + look nodes added
    + rotate next/behind functionality
    + LookNode prefab
    + node currently only stores behind and next nodes

---
+++ BurntOut_Unity/Assets/Scripts/CameraFunctionality/LookNode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookNode : MonoBehaviour {
+
+    public int id;
+    public LookNode next;
+    public LookNode behind;
+
+    private WorldSpaceUI ws_ui;
+
+    private Camera playerCamera;
+    private PlayerRotateToTarget myRotate;
+
+	// Use this for initialization
+	void Start () {
+
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        myRotate = playerCamera.GetComponent<PlayerRotateToTarget>();
+        ws_ui = GameObject.FindGameObjectWithTag("WSUI").GetComponent<WorldSpaceUI>();
+
+        if (id == 0) {
+            myRotate.enabled = true;
+            myRotate.target = this.gameObject;
+            ws_ui.current = this;
+        }
+
+	}
+
+    public void RotateNext() {
+        myRotate.target = next.gameObject;
+    }
+
+    public void RotateBehind() {
+        myRotate.target = behind.gameObject;
+    }
+
+}
---
+++ BurntOut_Unity/Assets/Scripts/CameraFunctionality/WorldSpaceUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSpaceUI : MonoBehaviour {
+
+    public GameObject player;
+    public Camera playerCamera;
+
+    public float rotationSpeed = 1f;
+    public bool rotateCameraRight;
+
+    public LookNode current;
+
+    public void RotateNext() {
+        current.RotateNext();
+        current = current.next;
+    }
+
+    public void RotateBehind() {
+        current.RotateBehind();
+        current = current.behind;
+    }
+
+
+
+}
