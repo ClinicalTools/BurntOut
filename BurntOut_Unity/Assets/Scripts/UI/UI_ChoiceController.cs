8771def836996acc40ec7ecb926942510250be81
voltairemedina <medina@clinicaltools.com>
2018-07-02 14:08:25 -0400

    Began choice button transition/fadeIn in diologue manager

---
+++ BurntOut_Unity/Assets/Scripts/UI/UI_ChoiceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ChoiceController : MonoBehaviour {
+
+    public Animator myanim;
+
+    private void OnEnable() {
+        myanim.SetBool("Choice", true);
+    }
+
+    private void OnDisable() {
+        myanim.SetBool("Choice", false);
+    }
+
+}
