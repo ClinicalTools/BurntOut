f14d665c0e74ec33cb82d08e68affbbbf76fd6a7
Chris Davis <davis@clincaltools.com>
2018-07-13 13:43:42 -0400

    Person correctly follows camera

---
+++ BurntOut_Unity/Assets/Scripts/CameraFunctionality/FollowCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.position = Camera.main.transform.position;
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
